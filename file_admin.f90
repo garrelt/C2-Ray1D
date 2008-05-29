1d8af4fe940dd5d06a073635619f08557941612f
garrelt <garrelt>
2008-05-29 19:32:20 +0000

    Doxygen comments added

--- file_admin.f90
+++ file_admin.f90
@@ -1,10 +1,24 @@
+!>
+!! \brief This module contains parameters having to do
+!!  with file in/ouput
+!!
+!! Module for C2-Ray/Capreole (f90)
+!!
+!! \b Author: Garrelt Mellema
+!!
+!! \b Date: 2008-05-27
+!!
+!! This module is also accepted by the F compiler (Dec 9, 2003)
+!<
 module file_admin
 
-  ! This module contains parameters having to do
-  ! with file in/ouput
+  implicit none
 
-  integer,public,parameter :: stdinput=5 ! set to not 5 if input via file (not implemented in all codes, check main program!)
-  integer,public,parameter :: logf=30     ! unit number of log file(s)
-  integer,public,parameter :: ah3=40     ! unit number of ah3 files (if applicable)
+  private
+  
+  !> set to not 5 if input via file (not implemented in all codes, check main program!)  
+  integer,public,parameter :: stdinput=5 
+  integer,public,parameter :: logf=30 !< unit number of log file(s)
+  integer,public,parameter :: ah3=40  !< unit number of ah3 files (if applicable)
 
 end module file_admin
