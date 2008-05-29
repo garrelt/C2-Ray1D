5f7194dc86946b2835a171ec28059a29507970f9
garrelt <garrelt>
2008-05-29 19:33:31 +0000

    Enabling doxygen documentation

---
+++ mainpage.h
@@ -0,0 +1,32 @@
+/** @mainpage C2-Ray (1D)
+*
+* @authors Garrelt Mellema & Ilian T. Iliev
+*
+* @section intro Introduction
+* This program does a 1-dimensional time-dependent photo-ionization
+* calculation for one problem from a series of test problems. 
+* These test problems are
+* the ones described in the C2-Ray paper:
+*
+* <em>C2-Ray: A new method for photon-conserving transport of ionizing radiation</em>\n
+* G. Mellema, I. Iliev, M. Alvarez, P. R. Shapiro, 2006\n
+* New Astronomy 11, 374
+*
+* No hydrodynamics is included.
+*
+* Sample input files can be found in the \c inputs directory
+*
+* The program needs cooling tables, which can be found in the
+* subdirectory \c tables
+*
+* <hr>
+* @section notes release.notes
+* release.notes
+* <hr>
+* @section requirements requirements
+* @verbinclude requirements
+* <hr> 
+* @todo [optionally include text about more work to be done]
+* @todo Give each todo item its own line
+*
+*/
