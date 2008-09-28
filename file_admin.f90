b830a282452afc5e0392a5ec6bcdd997c270276e
garrelt <garrelt>
2008-09-28 19:48:39 +0000

    Added extra variables and routines for flagging input through file.

--- file_admin.f90
+++ file_admin.f90
@@ -1,5 +1,5 @@
 !>
-!! \brief This module contains parameters having to do
+!! \brief This module contains parameters and routines having to do
 !!  with file in/ouput
 !!
 !! Module for C2-Ray/Capreole (f90)
@@ -15,10 +15,29 @@ module file_admin
   implicit none
 
   private
-  
+
+  !> directory where files with results are placed
+  character(len=50),public,parameter :: results_dir="../results/"
+
   !> set to not 5 if input via file (not implemented in all codes, check main program!)  
   integer,public,parameter :: stdinput=5 
   integer,public,parameter :: logf=30 !< unit number of log file(s)
   integer,public,parameter :: ah3=40  !< unit number of ah3 files (if applicable)
+  logical,public :: file_input=.false. !< input from file?
+
+  public :: flag_for_file_input
+
+contains
+  
+  !> Set the file_input logical to distinguish between input
+  !! through file from input through standard input 
+  subroutine flag_for_file_input(flag)
+
+    logical,intent(in) :: flag !< true if input from file
+
+    ! Sets flag for inpit via file (and not via std input)
+    file_input=flag
+    
+  end subroutine flag_for_file_input
 
 end module file_admin
